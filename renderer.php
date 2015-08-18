deef28b1a6e63ffdde9e7303ff966a3ec576902f
Thomas Threadgold <tj.threadgold@gmail.com>
2015-08-18 16:07:47 +1200

    Updated renderer to output course grid HTML

--- renderer.php
+++ renderer.php
@@ -10,8 +10,113 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-class block_my_courses_renderer extends plugin_renderer_base {
+class block_my_course_progress_renderer extends plugin_renderer_base {
+
+	/**
+	 * Output the HTML grid of courses
+	 * @param  array 	$courses 	the list of enrolled courses
+	 * @return string          		HTML fragment
+	 */
+	function course_grid($courses) {
+		global $CFG;
+		require_once($CFG->dirroot.'/blocks/my_course_progress/locallib.php');
+
+		$html = sprintf('<div class="grid__container">');
+
+		// LOOP THROUGH EACH COURSE AND GET THE OUTPUT
+		foreach ($courses as $c) {
+			
+			// OPEN GRID ITEM
+			$html .= sprintf('<div class="grid__item">');
+
+				// RENDER THE COURSE DETAILS
+				$html .= $this->course($c);
+
+				// IF COURSE HAS COMPLETION STATS, DO
+				if ( !!$c->enablecompletion ) {
+
+					// TODO: GET COMPLETION PERCENTAGE
+					$percentage = block_my_course_progress_get_completion_percentage((int)$c->id);
+
+					// RENDER THE COURSE PROGRESS
+					$html .= $this->progress($percentage);
+				}
+
+			// CLOSE GRID ITEM
+			$html .= sprintf('</div>');
+
+		}
+
+		return $html;
+	}
 
 
+	/**
+	 * Output the course HTML string
+	 * @param  course 	$c		Moodle course object
+	 * @return string     		HTML fragment
+	 */
+	function course($c) {
+
+		// OPEN COURSE WRAPPER
+		$html = sprintf('<div class="course">');
+			
+			// TODO: get course image url
+			$courseImageURL = '';
+
+			// OUTPUT COURSE IMAGE
+			$html .= sprintf('<img src="%s" class="course__image" alt="%s">',
+				$courseImageURL,
+				$c->shortname
+			);
+
+			// OUTPUT COURSE TITLE
+			$html .= sprintf('<h4 class="course__title"><a class="course__link" href="%s">%s</a></h4>',
+				new moodle_url('/course/view.php', array('id' => $c->id)),
+				$c->fullname
+			);
+
+		// CLOSE COURSE WRAPPER
+		$html .= sprintf('</div>');
+
+		return $html;
+	}
+
+
+	/**
+	 * Output the progress HTML string
+	 * @param  float  	$percentage 	the completion percentage
+	 * @return string             		HTML fragment
+	 */
+	function progress($percentage) {
+
+		// OPEN PROGRESS WRAPPER
+		$html = sprintf('<div class="progress-wrapper">');
+
+			// OUTPUT COMPLETION STATUS
+			if( $percentage < 100) {
+				// OUTPUT PERCENTAGE LABEL
+				$html .= sprintf('<div class="progress__completion %s">%s <span class="progress__percent">%.0f</span></div>',
+					get_string('completion_label'),
+					$percentage
+				);
+
+				// OUTPUT PROGRESS BAR
+				$html .= sprintf('<div class="progress__bar"><div class="progress__bar__fill" style="width: %.0f%%;"></div></div>',
+					$percentage
+				);
+
+			} else {
+				// OUTPUT COMPLETED LABEL
+				$html .= sprintf('<div class="progress__completion status--completed">%s</div>',
+					get_string('completion_finished_label')
+				);
+			}
+
+		// CLOSE PROGRESS WRAPPER
+		$html .= sprintf('</div>');
+
+		return $html;
+	}
 
 }
